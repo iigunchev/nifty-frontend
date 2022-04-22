abbc6019829c7eafe5cc041aa50f29974e1e198d
Luis Molina <luismateo2416@gmail.com>
2022-04-22 18:08:55 +0200

    reducer user, login btn test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
+import { useSelector, useDispatch } from 'react-redux';
+
+import { setUser } from './redux/User/userSlice';
+
 function App() {
-  return (
-    <>
-      <h1>hola panas</h1>
-      <section></section>
-    </>
-  );
+  return <div>App</div>;
 }
 
 export default App;
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from 'react';
 
-import "./Login.scss";
+import { useSelector, useDispatch } from 'react-redux';
+import { setUser } from '../../redux/User/userSlice';
+
+import './Login.scss';
 
 function Login() {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user);
+
+  const handleLogin = () => {
+    dispatch(
+      setUser({
+        email: 'sadasdasdasdlasda',
+        token: 'asd',
+        id: '4'
+      })
+    );
+  };
+
   return (
-    <div>Login</div>
-  )
+    <>
+      <button onClick={handleLogin}>login</button>
+      <span>{user?.email}</span>
+    </>
+  );
 }
 
-export default Login
+export default Login;
