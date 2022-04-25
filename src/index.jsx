64a9495a3a9fbb814f01ea10c03e6741e152d93e
Luis Molina <luismateo2416@gmail.com>
2022-04-25 10:10:10 +0200

    index js to jsx and provider removed

--- src/index.jsx
+++ src/index.jsx
@@ -4,15 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { store } from './redux/store';
-import { Provider } from 'react-redux';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
 
