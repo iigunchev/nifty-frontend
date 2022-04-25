30b1dcf7fd378020f9f0ee5cac1db7320f1fe18c
Luis Molina <luismateo2416@gmail.com>
2022-04-25 09:58:31 +0200

    eslint rules modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-
-import { useSelector, useDispatch } from 'react-redux';
+// react router dom
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // pages
 import Home from './pages/Home';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
-// react router dom
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { setUser } from './redux/User/userSlice';
 import * as route from './routes';
 
 function App() {
