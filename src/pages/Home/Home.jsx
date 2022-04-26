78ccd35b54f114abe1629e361f8bae8cdd4fdd95
Albert Garcia Fernandez <alberd_beto@hotmail.com>
2022-04-26 12:50:09 +0200

    Logo Created & Variable colors

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import LOGO from '../../assets/svg/LogoViolet.svg';
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      Home
+      <img src={LOGO} alt="nifty-logo" />
+    </div>
+  );
 }
 
 export default Home;
