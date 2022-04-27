274603ed0009272eea1b7139894a06055f53eaa2
Albert Garcia Fernandez <alberd_beto@hotmail.com>
2022-04-27 13:50:31 +0200

    Login 1st style created

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import { React } from 'react';
+
+import './Home.scss';
 
 import LOGO from '../../assets/svg/LogoViolet.svg';
+import LOGO2 from '../../assets/img/img1.jpg';
 
 function Home() {
   return (
     <div>
-      CAMARA DE LOS SECRETOS
-      <img src={LOGO} alt="nifty-logo" />
+      <ul className="menu">
+        <li>
+          <img src={LOGO} alt="nifty-logo" />
+        </li>
+        <li>
+          <a href="http://localhost:3000/login">Sign In</a>
+        </li>
+        <li>
+          <a href="http://localhost:3000/sign-up">START</a>
+        </li>
+      </ul>
+      <img src={LOGO2} alt="nifty-logo" />
     </div>
   );
 }
