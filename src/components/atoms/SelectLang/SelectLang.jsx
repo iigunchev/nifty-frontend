316bc37c598a58733f0f8442c71a724e963d175e
Luis Molina <luismateo2416@gmail.com>
2022-05-03 14:07:39 +0200

    i18n with localStorage

--- src/components/atoms/SelectLang/SelectLang.jsx
+++ src/components/atoms/SelectLang/SelectLang.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 import './SelectLang.scss';
 import { useTranslation } from 'react-i18next';
 import { useDispatch } from 'react-redux';
-import Cookie from 'js-cookie';
 import { setLanguage } from '../../../redux/User/userSlice';
 // cookies
+// import Cookie from "js-cookie"
 function SelectLang() {
   const { i18n } = useTranslation();
   const dispatch = useDispatch();
@@ -13,12 +13,14 @@ function SelectLang() {
     const language = e.target.value;
     i18n.changeLanguage(language);
     dispatch(setLanguage(language));
-    Cookie.set('i18next', language);
+    // cookie.set("i18next",language);
+    localStorage.setItem('lang', language);
   };
   return (
     <select
       className="selectLanguage"
       name="language"
+      defaultValue={localStorage.getItem('lang')}
       onChange={changeLanguageHandler}
     >
       <option value="en">EN</option>
--- src/index.jsx
+++ src/index.jsx
@@ -4,27 +4,24 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistStore } from 'redux-persist';
 
-import { I18nextProvider } from 'react-i18next';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 import './sass/main.scss';
 import store from './redux/store';
 // i18 languages
-import i18n from './i18n';
+import './i18n';
 
 const persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <I18nextProvider i18n={i18n}>
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <App />
-        </PersistGate>
-      </Provider>
-    </I18nextProvider>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
 
