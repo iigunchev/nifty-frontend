dc9ea612ef4f825f4bb5c03be733c1b2e76ab325
iigunchev <iigunchev@gmail.com>
2022-05-03 17:11:15 +0200

    trending item component

--- src/components/molecules/TrendingItem/TrendingItem.jsx
+++ src/components/molecules/TrendingItem/TrendingItem.jsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import ButtonWithIcon from '../ButtonWithIcon/ButtonWithIcon';
 
 import './TrendingItem.scss';
-import { ReactComponent as SVG } from '../../../assets/svg/verticalDots.svg';
 
-function TrendingItem({ artistImg, trackName, artistName }) {
+function TrendingItem({ image, title, description }) {
   return (
     <div className="trendingItemContainer">
-      {/* <span className="trendingSpot">{spot}</span> */}
-      <img src={artistImg} alt={`${artistName} ${trackName}`} />
+      <img src={image} alt={`${title}`} className="trendingItemImg" />
       <div className="detailsWrapper">
-        <span className="detailsName">{trackName}</span>
-        <div className="detailsOthers">
-          <span>{artistName}</span>
+        <span className="detailsTitle">{title}</span>
+        <div className="detailsDescription">
+          <span>{description}</span>
           {/* <span>Flamenco</span> */}
         </div>
       </div>
-      {/* <span>{trackDuration}</span> */}
-      <ButtonWithIcon />
-      <button type="button" title="More options">
-        <SVG className="verticalDots" />
-      </button>
     </div>
   );
 }
