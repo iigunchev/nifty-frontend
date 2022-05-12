3cc9b8e74b2f91f7676112e5567a199b17c3ce4c
iigunchev <iigunchev@gmail.com>
2022-05-12 11:49:16 +0200

    alternative player styles

--- src/components/molecules/ButtonWithIcon/ButtonWithIcon.jsx
+++ src/components/molecules/ButtonWithIcon/ButtonWithIcon.jsx
@@ -6,7 +6,7 @@ import { ReactComponent as SVG } from '../../../assets/svg/play.svg';
 function ButtonWithIcon({ handleClick }) {
   return (
     <div className="playButtonWrapper">
-      <button onClick={handleClick} type="button" className="buttonWithIcon">
+      <button onClick={handleClick} type="button">
         <SVG className="icon" />
       </button>
     </div>
