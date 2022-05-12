8defc343e76511a3d0fcc990aeb17dcd5124925b
Luis Molina <luismateo2416@gmail.com>
2022-05-12 18:10:31 +0200

    playlist styles remake

--- src/components/molecules/PlaylistItem/PlaylistItem.jsx
+++ src/components/molecules/PlaylistItem/PlaylistItem.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 
 import './PlaylistItem.scss';
 // icons
+import defaultImage from '../../../assets/img/defaultSong.png';
 import play from '../../../assets/img/player/play.png';
 import song from '../../../assets/svg/asideSvg/genresFilled.svg';
 
 function PlaylistItem({ name, tracksLength, image }) {
   return (
     <div className="playListItemContainer">
-      <img src={image} className="playlistImage" alt="playlistBackground" />
+      <img
+        src={image || defaultImage}
+        className="playlistImage"
+        alt="playlistBackground"
+      />
       <div className="playListBottomContainer">
         <div className="detailsWrapper">
           <span className="detailTitle">{name}</span>
