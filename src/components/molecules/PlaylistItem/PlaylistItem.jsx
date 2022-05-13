8ee0fa2532c3bae9cefcfa9c46e2b69b5aab67b5
Luis Molina <luismateo2416@gmail.com>
2022-05-13 09:48:05 +0200

    navigate playlist start

--- src/components/molecules/PlaylistItem/PlaylistItem.jsx
+++ src/components/molecules/PlaylistItem/PlaylistItem.jsx
@@ -1,4 +1,7 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react';
+// navigate
+import { useNavigate } from 'react-router-dom';
 
 import './PlaylistItem.scss';
 // icons
@@ -6,9 +9,17 @@ import defaultImage from '../../../assets/img/defaultSong.png';
 import play from '../../../assets/img/player/play.png';
 import song from '../../../assets/svg/asideSvg/genresFilled.svg';
 
-function PlaylistItem({ name, tracksLength, image }) {
+function PlaylistItem({ name, tracksLength, image, id = 0 }) {
+  const navigate = useNavigate();
   return (
-    <div className="playListItemContainer">
+    <div
+      onClick={() => {
+        navigate(id);
+      }}
+      role="button"
+      tabIndex={0}
+      className="playListItemContainer"
+    >
       <img
         src={image || defaultImage}
         className="playlistImage"
@@ -22,7 +33,13 @@ function PlaylistItem({ name, tracksLength, image }) {
             <span>{tracksLength} tracks</span>
           </div>
         </div>
-        <button type="button" className="playListActionButton">
+        <button
+          onClick={(e) => {
+            e.stopPropagation();
+          }}
+          type="button"
+          className="playListActionButton"
+        >
           <img className="filteredImg" src={play} alt="play" />
         </button>
       </div>
