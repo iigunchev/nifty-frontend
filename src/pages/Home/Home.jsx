986fb68315dc4477c9fbfa1605c37cdb54206c88
Luis Molina <luismateo2416@gmail.com>
2022-05-18 14:23:57 +0200

    carousel working

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-
-import PlaylistsList from '../../components/organism/PlaylistsList/PlaylistsList';
-
-// import TrendingItem from '../../components/molecules/TrendingItem/TrendingItem';
-
+// Carousel playlist
+import AliceCarousel from 'react-alice-carousel';
+// components
+import PlaylistItem from '../../components/molecules/PlaylistItem/PlaylistItem';
+// custom hook
 import useFetchItems from '../../hooks/useFetchItems';
 
 import TrendingTrackItemSkeleton from '../../components/molecules/Skeletons/TrendingTrackItemSkeleton';
+// styles
+import 'react-alice-carousel/lib/scss/alice-carousel.scss';
 import './Home.scss';
+// components
 import ArtistList from '../../components/organism/ArtistList/ArtistList';
 import TrendingList from '../../components/organism/TrendingList/TrendingList';
 import TrendingItemSkeleton from '../../components/molecules/Skeletons/TrendingItemSkeleton';
@@ -19,25 +22,56 @@ function Home() {
   const [artists, isLoadingArtists] = useFetchItems('account/byartist');
   const [playlists, isLoadingPlaylists] = useFetchItems('playlist');
 
+  const responsiveCarousel = {
+    0: { items: 2 },
+    568: { items: 3 },
+    900: { items: 4 },
+    1024: { items: 4 },
+    1500: { items: 5 }
+  };
+
+  const CarouselItems = () =>
+    playlists.map((playlist) => (
+      <div
+        key={playlist._id}
+        style={{ marginRight: 24 }}
+        className="carouselWrapper"
+      >
+        <PlaylistItem
+          id={playlist._id}
+          name={playlist.name}
+          tracks={playlist.tracks}
+          image={playlist.thumbnail}
+        />
+      </div>
+    ));
   return (
     <main className="homeContainer">
       <section className="trendingPlaylistsContainer">
         <div className="LinkHeader">
           <h2 className="heading2">Top PlayList</h2>
         </div>
-        <section className="PlayListSection">
-          {!isLoadingPlaylists ? (
-            <PlaylistsList playlists={playlists} />
-          ) : (
-            <TrendingItemSkeleton />
-          )}
-        </section>
+
+        {!isLoadingPlaylists ? (
+          <AliceCarousel
+            disableButtonsControls
+            autoPlay
+            autoPlayInterval={1500}
+            mouseTracking
+            responsive={responsiveCarousel}
+            infinite
+            items={CarouselItems()}
+            controlsStrategy="alternate"
+          />
+        ) : (
+          <TrendingItemSkeleton />
+        )}
       </section>
       <section className="homeBottomCol">
         <section className="trendingTracksContainer">
           <h2 className="heading2">Top Tracks</h2>
           {!isLoading ? (
-            <TrendingList tracks={songs?.slice(0, 6)} />
+            <TrendingList tracks={songs?.slice(0, 5)} />
           ) : (
             <TrendingTrackItemSkeleton />
           )}
@@ -45,7 +79,7 @@ function Home() {
         <section className="trendingArtistsContainer">
           <h2 className="heading2">Top Artists</h2>
           {!isLoadingArtists ? (
-            <ArtistList artists={artists?.slice(0, 6)} />
+            <ArtistList artists={artists?.slice(0, 5)} />
           ) : (
             <TrendingItemSkeleton />
           )}
