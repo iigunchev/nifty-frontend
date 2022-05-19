7818c64c371783c487fe1849893123cdbe252668
Luis Molina <luismateo2416@gmail.com>
2022-05-19 23:58:22 +0200

    redirect with google provider firebase

--- src/services/auth/auth.js
+++ src/services/auth/auth.js
@@ -7,7 +7,6 @@ import {
   signInWithEmailAndPassword,
   signOut,
   GoogleAuthProvider,
-  signInWithPopup,
   sendPasswordResetEmail,
   deleteUser,
   updateEmail,
@@ -31,11 +30,9 @@ initializeApp(firebaseConfig);
 
 export const auth = getAuth();
 
-export function signInWithGoogle(isMobile) {
+export function signInWithGoogle() {
   const GoogleProvider = new GoogleAuthProvider();
-  return isMobile
-    ? signInWithRedirect(auth, GoogleProvider)
-    : signInWithPopup(auth, GoogleProvider);
+  return signInWithRedirect(auth, GoogleProvider);
 }
 
 export function signInEmailAndPassword(email, password) {
